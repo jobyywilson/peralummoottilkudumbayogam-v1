2b6760e90199c5930baf2bfdd2ffa644b3e0fbc6
Joby Wilson Mathews <jobyywilson@gmail.com>
2023-11-24 07:08:24 +0530

    change image source

--- src/app/service/image.service.ts
+++ src/app/service/image.service.ts
@@ -39,10 +39,10 @@ public getUserPhotoUrl(userId:string){
         let profilePic = "assets/img/user.png"
         let spousePic =  "assets/img/user.png"
           if(photoName){
-            profilePic = "https://raw.githubusercontent.com/jobyywilson/peralummoottil-resource/main/"+photoName;
+            profilePic = "https://family-search-image.netlify.app/"+photoName;
         }
         if(spousePhotoName){
-          spousePic = "https://raw.githubusercontent.com/jobyywilson/peralummoottil-resource/main/"+spousePhotoName;
+          spousePic = "https://family-search-image.netlify.app/"+spousePhotoName;
         }else{
           spousePic = "assets/img/user.png";
         }
